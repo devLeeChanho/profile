a366acd02cf6c2d2bf5383eece4588f8183ad74b
DevLeeChanho <0813mason@gmail.com>
2018-02-15 02:24:31 +0900

    폰트 import, 로딩 섹션 추가

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,43 @@
 <template>
   <div id="app">
-    <h1 class="temp-head">ABCDEFGHIJKLMNOPQRSTUVW</h1>
-    <img src="./assets/logo.png" alt="">
+    <app-loading></app-loading>
+    <global-nav></global-nav>
+    <app-main></app-main>
+    <profile-section></profile-section>
+    <skill-set></skill-set>
+    <project-student></project-student>
+    <project-employee></project-employee>
   </div>
 </template>
 
 <script>
+import AppLoading from "./components/AppLoading.vue";
+import GlobalNav from "./components/GlobalNav.vue";
+import AppMain from "./components/AppMain.vue";
+import ProfileSection from "./components/ProfileSection.vue";
+import SkillSet from "./components/SkillSet.vue";
+import ProjectStudent from "./components/ProjectStudent.vue";
+import ProjectEmployee from "./components/ProjectEmployee.vue";
 
 export default {
   name: 'App',
   components: {
-
+    "app-loading": AppLoading,
+    "global-nav": GlobalNav,
+    "app-main": AppMain,
+    "profile-section": ProfileSection,
+    "skill-set": SkillSet,
+    "project-student": ProjectStudent,
+    "project-employee": ProjectEmployee
+  },
+  created() {
+    // 브라우저 크기 최대화 메소드
+    this.browserMaximize();
+  },
+  methods: {
+    browserMaximize(){
+      
+    }
   }
 }
 </script>
---
+++ src/components/AppLoading.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="loading">
+    <div class="loading-container">
+      <img src="../assets/logo.png" alert="뷰 로고">
+      <p class="loading-notice"> 
+        조금만 기다려주세요.<br>
+        Please wait a minute.
+      </p>
+      <div class="user-info">
+        <span class="browser-check">
+          {{browser}}
+          <i v-bind:class="{ support : browserSupport }"></i>
+        </span>
+        <span class="resulution-check">
+          {{resolution}}
+          <i class="support"></i>
+        </span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      browserSupport: true // 지원하는 브라우저 버전인지 체크
+    }
+  },
+  computed: {
+    browser: function() { // 브라우저 정보
+      let agent = navigator.userAgent, match;
+      let app, version;
+
+      if((match = agent.match(/MSIE ([0-9]+)/)) || (match = agent.match(/Trident.*rv:([0-9]+)/))) app = 'Internet Explorer';
+      else if(match = agent.match(/Edge\/([0-9]+)/)) app = 'Edge';
+      else if(match = agent.match(/Chrome\/([0-9]+)/)) app = 'Chrome';
+      else if(match = agent.match(/Firefox\/([0-9]+)/)) app = 'Firefox';
+      else if(match = agent.match(/Safari\/([0-9]+)/)) app = 'Safari';
+      else if((match = agent.match(/OPR\/([0-9]+)/)) || (match = agent.match(/Opera\/([0-9]+)/))) app = 'Opera';
+      else app = 'Unknown';
+
+      if(app !== 'Unknown') version = match[1];
+
+      //  버전 별 support icon 색상 변경
+      //  IE 9버전 이하를 제외한 나머지 브라우저 버전 지원
+      if(app === "Internet Explorer"){
+        if(version <= 9){
+          console.log("IE 9이하 버전")
+          this.browserSupport = false;
+        }
+      }
+
+      return app+" "+version;
+    },
+    resolution: function() { // 헤상도 정보
+      return screen.width+" X "+screen.height;
+    }
+  }
+}
+</script>
